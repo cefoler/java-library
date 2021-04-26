7da567bb59cce4f31a90f692897946753b7a9bf5
mluizaa00 <taeckls@gmail.com>
2021-04-26 14:36:07 -0300

    Added BorderShape to help in Menu development

---
+++ spigot/src/main/java/com/celeste/library/spigot/util/menu/BorderShape.java
@@ -0,0 +1,35 @@
+package com.celeste.library.spigot.util.menu;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum BorderShape {
+
+  // Inventory with 27 slots
+  THREE(new Integer[] {
+      10, 11, 12, 13, 14, 15, 16
+  }),
+  // Inventory with 36 slots
+  FOUR(new Integer[] {
+      10, 11, 12, 13, 14, 15, 16,
+      19, 20, 21, 22, 23, 24, 25
+  }),
+  // Inventory with 46 slots
+  FIVE(new Integer[] {
+      10, 11, 12, 13, 14, 15, 16,
+      19, 20, 21, 22, 23, 24, 25,
+      28, 29, 30, 31, 32, 33, 34
+  }),
+  // Inventory with 54 slots
+  SIX(new Integer[] {
+      10, 11, 12, 13, 14, 15, 16,
+      19, 20, 21, 22, 23, 24, 25,
+      28, 29, 30, 31, 32, 33, 34,
+      37, 38, 39, 40, 41, 42, 43
+  });
+
+  private final Integer[] shape;
+
+}
