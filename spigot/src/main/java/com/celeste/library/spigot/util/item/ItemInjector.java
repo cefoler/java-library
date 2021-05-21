d7bef704719e5465e005a4f6f385f89288e60f8c
DeserRC <DeserRC@gmail.com>
2021-05-21 06:16:02 -0300

    Added ItemInjector

--- spigot/src/main/java/com/celeste/library/spigot/util/item/ItemInjector.java
+++ spigot/src/main/java/com/celeste/library/spigot/util/item/ItemInjector.java
@@ -1,91 +1,173 @@
 package com.celeste.library.spigot.util.item;
 
+import com.celeste.library.core.util.Reflection;
+import com.celeste.library.spigot.error.ServerStartError;
 import com.celeste.library.spigot.util.ReflectionNms;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import net.minecraft.server.v1_8_R3.NBTTagCompound;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.Nullable;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 public final class ItemInjector {
+
+  private static final Constructor<?> COMPOUND_CONSTRUCTOR;
+
+  private static final Constructor<?> STRING_CONSTRUCTOR;
+  private static final Constructor<?> DOUBLE_CONSTRUCTOR;
+  private static final Constructor<?> INT_CONSTRUCTOR;
+
+  private static final Method AS_NMS_COPY;
+  private static final Method HAS_TAG;
+  private static final Method GET_TAG;
+
+  private static final Method GET_STRING;
+  private static final Method GET_DOUBLE;
+  private static final Method GET_INT;
+
+  private static final Method SET;
+  private static final Method SET_TAG;
+  private static final Method GET_ITEM_META;
+
+  private static final Method REMOVE;
+
+  private static final Method HAS_KEY;
+
+  static {
+    try {
+      final Class<?> craftItemStackClazz = ReflectionNms.getObc("inventory.CraftItemStack");
+      final Class<?> itemStackClazz = ReflectionNms.getNms("ItemStack");
+      final Class<?> compoundClazz = ReflectionNms.getNms("NBTTagCompound");
+
+      COMPOUND_CONSTRUCTOR = Reflection.getConstructor(compoundClazz);
+
+      AS_NMS_COPY = Reflection.getMethod(craftItemStackClazz, "asNMSCopy", ItemStack.class);
+      HAS_TAG = Reflection.getMethod(itemStackClazz, "hasTag");
+      GET_TAG = Reflection.getMethod(itemStackClazz, "getTag");
+
+      // GET
+      GET_STRING = Reflection.getMethod(compoundClazz, "getString", String.class);
+      GET_DOUBLE = Reflection.getMethod(compoundClazz, "getInt", String.class);
+      GET_INT = Reflection.getMethod(compoundClazz, "getDouble", String.class);
+
+      // SET
+      final Class<?> baseClazz = ReflectionNms.getNms("NBTBase");
+      final Class<?> stringClazz = ReflectionNms.getNms("NBTTagString");
+      final Class<?> doubleClazz = ReflectionNms.getNms("NBTTagDouble");
+      final Class<?> intClazz = ReflectionNms.getNms("NBTTagInt");
+
+      STRING_CONSTRUCTOR = Reflection.getConstructor(stringClazz, String.class);
+      DOUBLE_CONSTRUCTOR = Reflection.getConstructor(doubleClazz, double.class);
+      INT_CONSTRUCTOR = Reflection.getConstructor(intClazz, int.class);
+
+      SET = Reflection.getMethod(compoundClazz, "set", String.class, baseClazz);
+      SET_TAG = Reflection.getMethod(itemStackClazz, "setTag", compoundClazz);
+      GET_ITEM_META = Reflection.getMethod(craftItemStackClazz, "getItemMeta", itemStackClazz);
+
+      // REMOVE
+      REMOVE = Reflection.getMethod(compoundClazz, "remove", String.class);
+
+      // HAS
+      HAS_KEY = Reflection.getMethod(compoundClazz, "hasKey", String.class);
+    } catch (Exception exception) {
+      throw new ServerStartError(exception.getMessage(), exception.getCause());
+    }
+  }
 
   private final ItemStack item;
 
-  private final Class<?> ctsClass;
-  private final Class<?> isClass;
-  private final Class<?> NtcClass;
+  private String getString(final String key) {
+    final Object object = get(GET_STRING, key);
 
-  private final Method asNmsCopy;
-  private final Method hasTag;
-  private final Method getTag;
-  private final Method getString;
+    return object != null ? (String) object : "";
+  }
 
+  private double getDouble(final String key) {
+    final Object object = get(GET_DOUBLE, key);
+
+    return object != null ? (double) object : 0;
+  }
+
+  private int getInteger(final String key) {
+    final Object object = get(GET_INT, key);
+
+    return object != null ? (int) object : 0;
+  }
+
+  @Nullable
   @SneakyThrows
-  public ItemInjector(final ItemStack item) {
-    this.item = item;
+  private Object get(final Method getNbtTag, final String key) {
+    final Object nmsItem = Reflection.invokeStatic(AS_NMS_COPY, item);
+    final boolean containsTag = (boolean) Reflection.invoke(HAS_TAG, nmsItem);
 
-    ctsClass = ReflectionNms.getObc("inventory.CraftItemStack");
-    isClass = ReflectionNms.getNms("ItemStack");
-    NtcClass = ReflectionNms.getNms("NBTTagCompound");
+    if (!containsTag) {
+      return null;
+    }
 
-    asNmsCopy = ReflectionNms.getMethod(ctsClass, "asNMSCopy", ItemStack.class);
-    hasTag = ReflectionNms.getMethod(isClass, "hasTag");
-    getTag = ReflectionNms.getMethod(isClass, "getTag");
-    getString = ReflectionNms.getMethod(NtcClass, "getString", String.class);
+    final Object compound = Reflection.invoke(GET_TAG, nmsItem);
+    return Reflection.invoke(getNbtTag, compound, key);
+  }
+
+  public void setString(final String key, final String value) {
+    set(STRING_CONSTRUCTOR, key, value);
+  }
+
+  public void setDouble(final String key, final double value) {
+    set(DOUBLE_CONSTRUCTOR, key, value);
+  }
+
+  public void setInteger(final String key, final int value) {
+    set(INT_CONSTRUCTOR, key, value);
   }
 
   @SneakyThrows
-  public String get(final String key) {
-    final Object nmsItem = invokeStatic(asNmsCopy, item);
-    final Object compound = invoke(getTag, nmsItem);
+  private void set(final Constructor<?> nbtTagConstructor, final String key, final Object value) {
+    final Object nmsItemStack = Reflection.invokeStatic(AS_NMS_COPY, item);
+    final boolean containsTag = (Boolean) Reflection.invoke(HAS_TAG, nmsItemStack);
 
-    return getString.invoke(compound, key).toString();
+    final Object compound = containsTag
+        ? Reflection.invoke(GET_TAG, nmsItemStack)
+        : Reflection.instance(COMPOUND_CONSTRUCTOR);
+
+    final Object tag = Reflection.instance(nbtTagConstructor, value);
+    Reflection.invoke(SET, compound, key, tag);
+    Reflection.invoke(SET_TAG, nmsItemStack, compound);
+
+    final ItemMeta meta = (ItemMeta) Reflection.invokeStatic(GET_ITEM_META, nmsItemStack);
+    item.setItemMeta(meta);
   }
 
   @SneakyThrows
-  public void set(@NotNull final String key, @NotNull final Object value) {
-    item.setItemMeta(item.getItemMeta());
+  private void remove(final String key) {
+    final Object nmsItem = Reflection.invokeStatic(AS_NMS_COPY, item);
+    final boolean containsTag = (boolean) Reflection.invoke(HAS_TAG, nmsItem);
 
-    final Class<?> craftItemStackClazz = ReflectionNms.getObc("inventory.CraftItemStack");
-    final Class<?> itemStackClazz = ReflectionNms.getNms("ItemStack");
-    final Class<?> compoundClazz = ReflectionNms.getNms("NBTTagCompound");
+    if (!containsTag) {
+      return;
+    }
 
-    final Method asNMSCopy = ReflectionNms
-        .getMethod(craftItemStackClazz, "asNMSCopy", ItemStack.class);
-    final Method hasTag = ReflectionNms.getMethod(itemStackClazz, "hasTag");
-    final Method getTag = ReflectionNms.getMethod(itemStackClazz, "getTag");
+    final Object compound = Reflection.invoke(GET_TAG, nmsItem);
+    Reflection.invoke(REMOVE, compound, key);
 
-    final Object nmsItem = ReflectionNms.invokeStatic(asNMSCopy, item);
-    final boolean isExist = (Boolean) ReflectionNms.invoke(hasTag, nmsItem);
-    final Object compound =
-        isExist ? ReflectionNms.invoke(getTag, nmsItem) : compoundClazz.newInstance();
-
-    final Class<?> tagClazz = ReflectionNms.getNms("NBTTagString");
-    final Class<?> baseClazz = ReflectionNms.getNms("NBTBase");
-
-    final Constructor<?> tagCon = ReflectionNms.getDcConstructor(tagClazz, String.class);
-
-    final Method set = ReflectionNms.getMethod(compoundClazz, "set", String.class, baseClazz);
-    final Method setTag = ReflectionNms.getMethod(itemStackClazz, "setTag", compoundClazz);
-    final Method getItemMeta = ReflectionNms
-        .getMethod(craftItemStackClazz, "getItemMeta", itemStackClazz);
-
-    final Object tag = ReflectionNms.instance(tagCon, value.toString());
-    ReflectionNms.invoke(set, compound, key, tag);
-    ReflectionNms.invoke(setTag, nmsItem, compound);
-
-    item.setItemMeta((ItemMeta) ReflectionNms.invokeStatic(getItemMeta, nmsItem));
+    final ItemMeta meta = (ItemMeta) Reflection.invokeStatic(GET_ITEM_META, nmsItem);
+    item.setItemMeta(meta);
   }
 
   @SneakyThrows
   public boolean has(final String key) {
-    final Object nmsItem = invokeStatic(asNmsCopy, item);
-    final Object compound = invoke(getTag, nmsItem);
-    final Object object = getString.invoke(compound, key);
+    final Object nmsItemStack = Reflection.invokeStatic(AS_NMS_COPY, item);
+    final boolean containsTag = (Boolean) Reflection.invoke(HAS_TAG, nmsItemStack);
 
-    return object != null;
+    if (!containsTag) {
+      return false;
+    }
+
+    final Object compound = Reflection.invoke(GET_TAG, nmsItemStack);
+    return (boolean) Reflection.invoke(HAS_KEY, compound, key);
   }
 
   public ItemStack inject() {
