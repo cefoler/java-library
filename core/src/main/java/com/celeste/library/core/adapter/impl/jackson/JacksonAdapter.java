2d9e9b8c559c6f570b2b3b5213b00ca158799801
mluizaa00 <luiza.prestes@uol.com.br>
2021-06-23 23:11:59 -0300

    added: node and get methods to JacksonAdapter

--- core/src/main/java/com/celeste/library/core/adapter/impl/jackson/JacksonAdapter.java
+++ core/src/main/java/com/celeste/library/core/adapter/impl/jackson/JacksonAdapter.java
@@ -3,7 +3,11 @@ package com.celeste.library.core.adapter.impl.jackson;
 import com.celeste.library.core.adapter.Json;
 import com.celeste.library.core.adapter.exception.JsonDeserializeException;
 import com.celeste.library.core.adapter.exception.JsonSerializeException;
+import com.celeste.library.core.util.Validation;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.SneakyThrows;
 
 public final class JacksonAdapter implements Json {
 
@@ -36,6 +40,39 @@ public final class JacksonAdapter implements Json {
     } catch (Exception exception) {
       throw new JsonDeserializeException(exception);
     }
+  }
+
+  public ObjectNode createNode() {
+    return mapper.createObjectNode();
+  }
+
+  @SneakyThrows
+  public ObjectNode getNode(final String json) {
+    return mapper.readValue(json, ObjectNode.class);
+  }
+
+  public JsonNode get(final ObjectNode node, final String key) {
+    return Validation.notNull(node.get(key), () -> new NullPointerException("The key specified cannot be null"));
+  }
+
+  public String getString(final ObjectNode node, final String key) {
+    return get(node, key).textValue();
+  }
+
+  public int getInt(final ObjectNode node, final String key) {
+    return get(node, key).intValue();
+  }
+
+  public boolean getBoolean(final ObjectNode node, final String key) {
+    return get(node, key).booleanValue();
+  }
+
+  public double getDouble(final ObjectNode node, final String key) {
+    return get(node, key).doubleValue();
+  }
+
+  public float getFloat(final ObjectNode node, final String key) {
+    return get(node, key).floatValue();
   }
 
   public static JacksonAdapter getInstance() {
