8123603ca96a60c6ee47a6d068b383edd6d11f44
mluizaa00 <luiza.prestes@uol.com.br>
2021-06-25 15:39:48 -0300

    update: cached multiple reflections on EntityInjector

--- spigot/src/main/java/com/celeste/library/spigot/util/EntityInjector.java
+++ spigot/src/main/java/com/celeste/library/spigot/util/EntityInjector.java
@@ -1,5 +1,6 @@
 package com.celeste.library.spigot.util;
 
+import com.celeste.library.core.util.Reflection;
 import lombok.SneakyThrows;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
@@ -9,10 +10,40 @@ import java.lang.reflect.Method;
 public final class EntityInjector {
 
   private static Class<?> TAG_CLASS;
+  private static Method GET_HANDLE;
+
+  private static Method GET_NBT_TAG;
+
+  private static Method GET_STRING;
+  private static Method GET_INT;
+  private static Method GET_DOUBLE;
+
+  private static Method SET_STRING;
+  private static Method SET_INT;
+  private static Method SET_DOUBLE;
+
+  private static Method C;
+  private static Method F;
 
   static {
     try {
       TAG_CLASS = ReflectionNms.getNms("NBTTagCompound");
+
+      final Class<?> craftEntityClazz = ReflectionNms.getObc("entity.CraftEntity");
+
+      GET_HANDLE = Reflection.getDcMethod(craftEntityClazz, "getHandle");
+      GET_NBT_TAG = Reflection.getDcMethod(GET_HANDLE.getClass(), "getNBTTag");
+
+      GET_STRING = TAG_CLASS.getMethod("getString", String.class);
+      GET_INT = TAG_CLASS.getMethod("getInt", String.class);
+      GET_DOUBLE = TAG_CLASS.getMethod("getDouble", String.class);
+
+      SET_STRING = TAG_CLASS.getMethod("setString", String.class, String.class);
+      SET_INT = TAG_CLASS.getMethod("setInt", String.class, int.class);
+      SET_DOUBLE = TAG_CLASS.getMethod("setDouble", String.class, double.class);
+
+      C = Reflection.getDcMethod(GET_HANDLE.getClass(), "c", TAG_CLASS);
+      F = Reflection.getDcMethod(GET_HANDLE.getClass(), "f", TAG_CLASS);
     } catch (ReflectiveOperationException exception) {
       exception.printStackTrace();
     }
@@ -25,58 +56,56 @@ public final class EntityInjector {
 
   @SneakyThrows
   public void set(final Method method, final Entity entity, final String key, Object value) {
-    final Object handle = entity.getClass().getMethod("getHandle").invoke(entity);
+    final Object handle = GET_HANDLE.invoke(entity);
     final Class<?> nmsEntity = handle.getClass();
 
-    final Object tag = nmsEntity.getMethod("getNBTTag").invoke(handle) == null
+    final Object tag = GET_NBT_TAG.invoke(handle) == null
         ? TAG_CLASS.getConstructor().newInstance()
-        : nmsEntity.getMethod("getNBTTag").invoke(handle);
+        : GET_NBT_TAG.invoke(handle);
 
-    nmsEntity.getMethod("c", TAG_CLASS).invoke(nmsEntity, tag);
+    C.invoke(nmsEntity, tag);
     method.invoke(tag, key, value);
-    nmsEntity.getMethod("f", TAG_CLASS).invoke(nmsEntity, tag);
+    F.invoke(nmsEntity, tag);
   }
 
   @SneakyThrows
   public void setInt(final Entity entity, final String key, int value) {
-    set(TAG_CLASS.getMethod("setInt", String.class, int.class), entity, key, value);
+    set(SET_INT, entity, key, value);
   }
 
   @SneakyThrows
   public void setString(final Entity entity, final String key, String value) {
-    set(TAG_CLASS.getMethod("setString", String.class, String.class), entity, key, value);
+    set(SET_STRING, entity, key, value);
   }
 
   @SneakyThrows
   public void setDouble(final Entity entity, final String key, double value) {
-    set(TAG_CLASS.getMethod("setDouble", String.class, double.class), entity, key, value);
+    set(SET_DOUBLE, entity, key, value);
   }
 
   @SneakyThrows
   public int getInt(final Entity entity, final String key) {
-    return (int) get("getInt", entity, key);
+    return (int) get(GET_INT, entity, key);
   }
 
   @SneakyThrows
   public String getString(final Entity entity, final String key) {
-    return (String) get("getString", entity, key);
+    return (String) get(GET_STRING, entity, key);
   }
 
   @SneakyThrows
   public double getDouble(final Entity entity, final String key) {
-    return (double) get("getDouble", entity, key);
+    return (double) get(GET_DOUBLE, entity, key);
   }
 
   @SneakyThrows
-  public Object get(final String method, final Entity entity, final String key) {
-    final Object handle = entity.getClass().getMethod("getHandle").invoke(entity);
-    final Class<?> nmsEntity = handle.getClass();
-
-    final Object tag = nmsEntity.getMethod("getNBTTag").invoke(handle) == null
+  public Object get(final Method method, final Entity entity, final String key) {
+    final Object handle = GET_HANDLE.invoke(entity);
+    final Object tag = GET_NBT_TAG.invoke(handle) == null
         ? TAG_CLASS.getConstructor().newInstance()
-        : nmsEntity.getMethod("getNBTTag").invoke(handle);
+        : GET_NBT_TAG.invoke(handle);
 
-    return TAG_CLASS.getMethod(method, String.class).invoke(tag, key);
+    return method.invoke(tag, key);
   }
 
 }
