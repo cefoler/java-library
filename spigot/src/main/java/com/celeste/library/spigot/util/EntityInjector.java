2a3cce8a734da7f3fdb378026e7238371d1cdaa7
mluizaa00 <luiza.prestes@uol.com.br>
2021-06-25 15:30:29 -0300

    added: nbt methods to EntityInjector and other methods to InventoryInjector

--- spigot/src/main/java/com/celeste/library/spigot/util/EntityInjector.java
+++ spigot/src/main/java/com/celeste/library/spigot/util/EntityInjector.java
@@ -1,7 +1,10 @@
 package com.celeste.library.spigot.util;
 
 import lombok.SneakyThrows;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
+
+import java.lang.reflect.Method;
 
 public final class EntityInjector {
 
@@ -10,21 +13,70 @@ public final class EntityInjector {
   static {
     try {
       TAG_CLASS = ReflectionNms.getNms("NBTTagCompound");
-    } catch (ClassNotFoundException exception) {
+    } catch (ReflectiveOperationException exception) {
       exception.printStackTrace();
     }
   }
 
   @SneakyThrows
   public void setAI(final LivingEntity entity) {
+    setInt(entity, "NoAI", 1);
+  }
+
+  @SneakyThrows
+  public void set(final Method method, final Entity entity, final String key, Object value) {
     final Object handle = entity.getClass().getMethod("getHandle").invoke(entity);
-    Object tag = handle.getClass().getMethod("getNBTTag").invoke(handle);
+    final Class<?> nmsEntity = handle.getClass();
 
-    if (tag == null) tag = TAG_CLASS.getConstructor().newInstance();
+    final Object tag = nmsEntity.getMethod("getNBTTag").invoke(handle) == null
+        ? TAG_CLASS.getConstructor().newInstance()
+        : nmsEntity.getMethod("getNBTTag").invoke(handle);
 
-    handle.getClass().getMethod("c", TAG_CLASS).invoke(handle, tag);
-    TAG_CLASS.getMethod("setInt", String.class, int.class).invoke(tag, "NoAI", 1);
-    handle.getClass().getMethod("f", TAG_CLASS).invoke(handle, tag);
+    nmsEntity.getMethod("c", TAG_CLASS).invoke(nmsEntity, tag);
+    method.invoke(tag, key, value);
+    nmsEntity.getMethod("f", TAG_CLASS).invoke(nmsEntity, tag);
+  }
+
+  @SneakyThrows
+  public void setInt(final Entity entity, final String key, int value) {
+    set(TAG_CLASS.getMethod("setInt", String.class, int.class), entity, key, value);
+  }
+
+  @SneakyThrows
+  public void setString(final Entity entity, final String key, String value) {
+    set(TAG_CLASS.getMethod("setString", String.class, String.class), entity, key, value);
+  }
+
+  @SneakyThrows
+  public void setDouble(final Entity entity, final String key, double value) {
+    set(TAG_CLASS.getMethod("setDouble", String.class, double.class), entity, key, value);
+  }
+
+  @SneakyThrows
+  public int getInt(final Entity entity, final String key) {
+    return (int) get("getInt", entity, key);
+  }
+
+  @SneakyThrows
+  public String getString(final Entity entity, final String key) {
+    return (String) get("getString", entity, key);
+  }
+
+  @SneakyThrows
+  public double getDouble(final Entity entity, final String key) {
+    return (double) get("getDouble", entity, key);
+  }
+
+  @SneakyThrows
+  public Object get(final String method, final Entity entity, final String key) {
+    final Object handle = entity.getClass().getMethod("getHandle").invoke(entity);
+    final Class<?> nmsEntity = handle.getClass();
+
+    final Object tag = nmsEntity.getMethod("getNBTTag").invoke(handle) == null
+        ? TAG_CLASS.getConstructor().newInstance()
+        : nmsEntity.getMethod("getNBTTag").invoke(handle);
+
+    return TAG_CLASS.getMethod(method, String.class).invoke(tag, key);
   }
 
 }
--- spigot/src/main/java/com/celeste/library/spigot/util/InventoryInjector.java
+++ spigot/src/main/java/com/celeste/library/spigot/util/InventoryInjector.java
@@ -25,6 +25,15 @@ public final class InventoryInjector {
         .collect(Collectors.toList());
   }
 
+  @Deprecated
+  public List<ItemStack> getItemsFromOldData(final Material material, int data, final Player player) {
+    return Arrays.stream(player.getInventory().getContents())
+        .filter(Objects::nonNull)
+        .filter(item -> item.getType() == material)
+        .filter(item -> item.getDurability() == data)
+        .collect(Collectors.toList());
+  }
+
   public int removeItemsFromMaterial(final Material material, final Player player) {
     final AtomicInteger count = new AtomicInteger();
     Arrays.stream(player.getInventory().getContents())
@@ -36,6 +45,28 @@ public final class InventoryInjector {
         });
 
     return count.get();
+  }
+
+  public void removeItemsFromMaterial(final Material material, int amount, final Player player) {
+    final AtomicInteger count = new AtomicInteger();
+    final List<ItemStack> items = Arrays.stream(player.getInventory().getContents())
+        .filter(Objects::nonNull)
+        .filter(item -> item.getType() == material)
+        .collect(Collectors.toList());
+
+    for (ItemStack item : items) {
+      final int newCount = count.get();
+      if (newCount + item.getAmount() > amount) {
+        final int amountLeft = newCount - amount;
+        item.setAmount(amountLeft);
+
+        player.getInventory().remove(item);
+        return;
+      }
+
+      count.set(newCount + item.getAmount());
+      player.getInventory().remove(item);
+    }
   }
 
 }
