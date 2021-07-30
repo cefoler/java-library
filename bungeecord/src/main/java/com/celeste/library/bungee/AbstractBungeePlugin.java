9a4e1563942a319073b2ce62b54ed582f73a2072
mluizaa00 <taeckls@gmail.com>
2021-07-30 16:28:45 -0300

    removed: CommandHolder at Bungee module, update: command framework

--- bungeecord/src/main/java/com/celeste/library/bungee/AbstractBungeePlugin.java
+++ bungeecord/src/main/java/com/celeste/library/bungee/AbstractBungeePlugin.java
@@ -1,14 +1,7 @@
 package com.celeste.library.bungee;
 
-import com.celeste.library.bungee.annotation.CommandHolder;
-import com.celeste.library.bungee.exception.InvalidCommandException;
-import com.celeste.library.bungee.exception.InvalidListenerException;
 import com.celeste.library.core.factory.ThreadingFactory;
-import com.celeste.library.core.util.Reflection;
-import java.lang.reflect.Constructor;
-import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.Arrays;
-import java.util.Map.Entry;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ScheduledExecutorService;
 import lombok.Getter;
@@ -18,7 +11,6 @@ import me.saiintbrisson.minecraft.command.message.MessageType;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.api.plugin.PluginManager;
-import org.reflections.Reflections;
 
 @Getter
 public abstract class AbstractBungeePlugin extends Plugin {
@@ -46,9 +38,9 @@ public abstract class AbstractBungeePlugin extends Plugin {
     final MessageHolder holder = frame.getMessageHolder();
 
     holder.setMessage(MessageType.ERROR, "§cA error occurred.");
-    holder.setMessage(MessageType.INCORRECT_TARGET, "Only {target} can execute this command..");
-    holder.setMessage(MessageType.INCORRECT_USAGE, "Wrong use! The correct is: /{usage}");
-    holder.setMessage(MessageType.NO_PERMISSION, "You don't have enough permissions.");
+    holder.setMessage(MessageType.INCORRECT_TARGET, "§cOnly {target} can execute this command..");
+    holder.setMessage(MessageType.INCORRECT_USAGE, "§cWrong use! The correct is: /{usage}");
+    holder.setMessage(MessageType.NO_PERMISSION, "§cYou don't have enough permissions.");
 
     frame.registerCommands(instances);
   }
@@ -63,103 +55,6 @@ public abstract class AbstractBungeePlugin extends Plugin {
     holder.setMessage(MessageType.NO_PERMISSION, messages[3]);
 
     frame.registerCommands(instances);
-  }
-
-  @Deprecated
-  public void register(final String path, final Class<?> clazz, final Object instance) {
-    registerListeners(path, clazz, instance);
-    registerCommands(path, clazz, instance);
-  }
-
-  @Deprecated
-  @SafeVarargs
-  public final void register(final String path, final Entry<Class<?>, Object>... entries) {
-    registerListeners(path, entries);
-    registerCommands(path, entries);
-  }
-
-  @Deprecated
-  public void registerListeners(final String path, final Class<?> clazz, final Object instance) {
-    registerListeners(path, new SimpleImmutableEntry<>(clazz, instance));
-  }
-
-  @Deprecated
-  @SafeVarargs
-  public final void registerListeners(final String path, final Entry<Class<?>, Object>... entries) {
-    try {
-      final Class<?>[] parameters = Arrays.stream(entries)
-          .map(Entry::getKey)
-          .toArray(Class[]::new);
-
-      final Object[] instances = Arrays.stream(entries)
-          .map(Entry::getValue)
-          .toArray();
-
-      final Reflections reflections = new Reflections(path);
-
-      for (final Class<? extends Listener> clazz : reflections.getSubTypesOf(Listener.class)) {
-        final Constructor<? extends Listener>[] constructors = Reflection.getConstructors(clazz);
-
-        final Constructor<? extends Listener> constructor = Arrays.stream(constructors)
-            .filter(newConstructor -> Arrays.equals(newConstructor.getParameterTypes(), parameters))
-            .findFirst()
-            .orElse(null);
-
-        if (constructor == null) {
-          continue;
-        }
-
-        manager.registerListener(this, constructor.newInstance(instances));
-      }
-    } catch (Exception exception) {
-      throw new InvalidListenerException("Unable to register listener: ", exception);
-    }
-  }
-
-  @Deprecated
-  public void registerCommands(final String path, final Class<?> clazz, final Object instance) {
-    registerCommands(path, new SimpleImmutableEntry<>(clazz, instance));
-  }
-
-  @Deprecated
-  @SafeVarargs
-  public final void registerCommands(final String path, final Entry<Class<?>, Object>... entries) {
-    try {
-      final Class<?>[] parameters = Arrays.stream(entries)
-          .map(Entry::getKey)
-          .toArray(Class[]::new);
-
-      final Object[] instances = Arrays.stream(entries)
-          .map(Entry::getValue)
-          .toArray();
-
-      final Reflections reflections = new Reflections(path);
-
-      final BungeeFrame frame = new BungeeFrame(this);
-      final MessageHolder holder = frame.getMessageHolder();
-
-      holder.setMessage(MessageType.ERROR, "§cA error occurred.");
-      holder.setMessage(MessageType.INCORRECT_TARGET, "§cOnly {target} can execute this command.");
-      holder.setMessage(MessageType.INCORRECT_USAGE, "§cWrong use! The correct is: /{usage}");
-      holder.setMessage(MessageType.NO_PERMISSION, "§cYou don't have enough permissions.");
-
-      for (final Class<?> clazz : reflections.getTypesAnnotatedWith(CommandHolder.class)) {
-        final Constructor<?>[] constructors = Reflection.getConstructors(clazz);
-
-        final Constructor<?> constructor = Arrays.stream(constructors)
-            .filter(newConstructor -> Arrays.equals(newConstructor.getParameterTypes(), parameters))
-            .findFirst()
-            .orElse(null);
-
-        if (constructor == null) {
-          continue;
-        }
-
-        frame.registerCommands(constructor.newInstance(instances));
-      }
-    } catch (Exception exception) {
-      throw new InvalidCommandException("Unable to register command: ", exception);
-    }
   }
 
   public static ExecutorService getExecutor() {
