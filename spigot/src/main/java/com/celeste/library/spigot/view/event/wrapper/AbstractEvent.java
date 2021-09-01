da4056c62180ce03bb67f9110ee6f0bb9565ec2d
mluizaa00 <taeckls@gmail.com>
2021-09-01 11:26:05 -0300

    added(AbstractEvent): dispatch method

--- spigot/src/main/java/com/celeste/library/spigot/view/event/wrapper/AbstractEvent.java
+++ spigot/src/main/java/com/celeste/library/spigot/view/event/wrapper/AbstractEvent.java
@@ -1,31 +1,31 @@
 package com.celeste.library.spigot.view.event.wrapper;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.PluginManager;
 
 public abstract class AbstractEvent extends Event {
 
   private static final HandlerList HANDLER_LIST;
+  private static final PluginManager MANAGER;
 
   static {
     HANDLER_LIST = new HandlerList();
+    MANAGER = Bukkit.getPluginManager();
   }
 
-  /**
-   * @return HandlerList
-   */
   @Override
   public HandlerList getHandlers() {
     return HANDLER_LIST;
   }
 
-  /**
-   * Static method to get the HandlerList
-   *
-   * @return HandlerList
-   */
   public static HandlerList getHandlerList() {
     return HANDLER_LIST;
+  }
+
+  public void dispatch() {
+    MANAGER.callEvent(this);
   }
 
 }
